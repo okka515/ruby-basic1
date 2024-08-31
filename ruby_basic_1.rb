8fd1f7bcbd1f4b7397db3722bfe79c398d0f3c63
EC2 Default User <ec2-user@ip-10-1-0-106.ap-northeast-1.compute.internal>
2024-08-31 02:22:08 +0000

    ruby基礎

---
+++ ruby_basic_1.rb
@@ -0,0 +1,10 @@
+#理想の女性について
+beautiful_face = true
+tall = 150
+age = 25
+
+if beautiful_face = true && tall <= 160 && age >= 18 && age <= 28
+    puts "理想の女性です!"
+else
+    puts "理想とは少し違うようです"
+end
